5c8c94dcfba2cf34f6bd6b84d8358ef69a55c41f
kohei nakashima <>
2023-07-18 10:30:17 +0900

    add JPY rate info

--- index.ts
+++ index.ts
@@ -5,10 +5,15 @@ const path = "docs/latest.json";
 
 request("https://rate.bot.com.tw/xrt/flcsv/0/day")
   .then(body => {
-    const usdInfo = body.split(/\r?\n/)[1];
+    const rateInfo = body.split(/\r?\n/);
+    const usdInfo = rateInfo[1];
+    const jpyInfo = rateInfo[8];
     return {
       dataAsOf: new Date().toLocaleDateString(),
-      conversions: { USD: { TWD: usdInfo.split(",")[3] } }
+      conversions: {
+        USD: { TWD: usdInfo.split(",")[3] },
+        JPY: { TWD: jpyInfo.split(",")[3] } 
+      }
     };
   })
   .then(data => promises.writeFile(path, JSON.stringify(data)));
